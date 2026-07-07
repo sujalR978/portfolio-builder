1294e034304780b61f95630cd0d0d32e8b51fd6c
sujalR978 <srashiya955@rku.ac.in>
2026-07-07 22:38:39 +0530

    #

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "hi how are you how are you"; // misktec changed
+    return "hi how are you how are you"; // mistake changed
 });
