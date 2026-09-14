31f4e9dab1c97d5ec030628eeb2a868ae7500d1f
sujalR978 <srashiya955@rku.ac.in>
2026-09-14 12:44:52 +0530

    #

--- resources/views/landing_page/log_in.blade.php
+++ resources/views/landing_page/log_in.blade.php
@@ -70,7 +70,7 @@
                     </div>
 
                     <!-- FORM -->
-                    <form action="{{ url('/log_in') }}" method="POST">
+                    <form action="{{ route('log_in') }}" method="POST">
                         @csrf
                         
                         <!-- Email Input -->
