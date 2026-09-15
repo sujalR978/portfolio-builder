6e4b1f2cb336b288b06a209d90b155862b8c5fa3
sujalR978 <srashiya955@rku.ac.in>
2026-09-15 15:38:56 +0530

    #

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -8,43 +9,41 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-   public function register(Request $request){
-  
-
+    public function register(Request $request)
+    {
         $request->validate([
             'firstName' => 'required|string|max:255',
             'lastName' => 'required|string|max:255',
-            'email' => 'required|string|unique:users,email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8|confirmed',
-            
-
         ]);
 
-        $user = User::create([
+        User::create([
             'firstName' => $request->firstName,
             'lastName' => $request->lastName,
             'email' => $request->email,
-            'password' =>$request->password,
+            'password' => Hash::make($request->password),
         ]);
 
-        return Redirect('/log_in')->with('success','Registration Successfully!');
-   }
-
-   public function Login(Request $request){
-
-    $credentials = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    if (Auth::attempt($credentials)) {
-        $request->session()->regenerate();
-
-        return redirect('/dashboard');
+        return redirect('/log_in')
+            ->with('success', 'Registration Successfully!');
     }
 
-    return back()->withErrors([
-        'email' => 'Email or password is incorrect.',
-    ])->onlyInput('email');
-   }
+    public function log_in(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email or password is incorrect.',
+        ])->onlyInput('email');
+    }
 }
