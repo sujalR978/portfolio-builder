bb06d15497e78a2ce7cd4f3027bd5a94f9c74d1f
sujalR978 <srashiya955@rku.ac.in>
2026-09-15 16:39:02 +0530

    #

--- resources/views/main/user_header.blade.php
+++ resources/views/main/user_header.blade.php
@@ -75,7 +75,7 @@
                             </li>
                             <li><hr class="dropdown-divider my-1"></li>
                             <li>
-                                <form action="{{ url('/log_out') }}" method="POST" class="m-0">
+                                <form action="{{ route('log_out') }}" method="POST" class="m-0">
                                     @csrf
                                     <button type="submit" class="dropdown-item rounded-3 small py-2 text-danger fw-semibold d-flex align-items-center gap-2">
                                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
